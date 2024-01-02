9f0ee3148a4890582585a24b4aef48fb0ebc8ac3
Milos Madro <madro.milos@gmail.com>
2024-01-02 19:53:25 +0100

    Inicializoval som projekt

---
+++ main.py
@@ -0,0 +1,7 @@
+list=list(range(100,999))
+b=0
+for i in list:
+  a = str(i)
+  if a[0] != a[1] and a[1] != a[2] and a[0] != a[2] or (a[0] == a[1] and a[1] == a[2] and a[0] == a[2]):
+  b=b+1
+print(b)
